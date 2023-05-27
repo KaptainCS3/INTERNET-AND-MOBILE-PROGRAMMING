19a6db495d7d5629eeb29ba95c79fa96342e24ec
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-27 21:15:25 +0100

    nav active state still pending

--- Task 5/Eat_Now/src/components/Nav.jsx
+++ Task 5/Eat_Now/src/components/Nav.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faBell, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faBell, faUser, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
   return (
     <nav className="fixed w-full bottom-4 left-0 right-0 z-10">
-      <section className="mx-4 border border-[#5DBA63] flex justify-between items-center py-2 px-4 rounded-2xl">
-        <div className="flex flex-col">
-          <FontAwesomeIcon icon={faHouse} />
+      <section className="mx-4 border bg-white shadow-xl border-[#5DBA63] flex justify-between items-center py-2 px-4 rounded-2xl">
+        <div className="flex flex-col cursor-pointer">
+          <FontAwesomeIcon icon={faHouse} className="text-[#5DBA63]"/>
           <span>Home</span>
         </div>
-        <div className="flex flex-col">
-          <FontAwesomeIcon icon={faBell} />
+        <div className="flex flex-col cursor-pointer">
+          <FontAwesomeIcon icon={faBell} className="text-[#5DBA63] "/>
           <span>Notification</span>
         </div>
-        <div className="flex flex-col">
-          {/* <FontAwesomeIcon icon={faCart} /> */}
+        <div className="flex flex-col cursor-pointer">
+          <FontAwesomeIcon icon={faShoppingCart} className="text-[#5DBA63] "/>
           <span>Cart</span>
         </div>
-        <div className="flex flex-col">
-          <FontAwesomeIcon icon={faUser} />
+        <div className="flex flex-col cursor-pointer">
+          <FontAwesomeIcon icon={faUser} className="text-[#5DBA63] "/>
           <span>Profile</span>
         </div>
       </section>
