15bf2888346ea04ea4065e3ec43cc3ab580aeb91
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-29 14:23:07 +0100

    added path to various routes

--- Task 5/Eat_Now/src/components/Nav.jsx
+++ Task 5/Eat_Now/src/components/Nav.jsx
@@ -1,27 +1,39 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faBell, faUser, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHouse,
+  faBell,
+  faUser,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className="fixed w-full bottom-4 left-0 right-0 z-10">
       <section className="mx-4 border bg-white shadow-xl border-[#5DBA63] flex justify-between items-center py-2 px-4 rounded-2xl">
+        <Link to="/home">
+          <div className="flex flex-col cursor-pointer">
+            <FontAwesomeIcon icon={faHouse} className="text-[#5DBA63]" />
+            <span>Home</span>
+          </div>
+        </Link>
+        <Link to="/notifications">
+          <div className="flex flex-col cursor-pointer">
+            <FontAwesomeIcon icon={faBell} className="text-[#5DBA63] " />
+            <span>Notification</span>
+          </div>
+        </Link>
         <div className="flex flex-col cursor-pointer">
-          <FontAwesomeIcon icon={faHouse} className="text-[#5DBA63]"/>
-          <span>Home</span>
-        </div>
-        <div className="flex flex-col cursor-pointer">
-          <FontAwesomeIcon icon={faBell} className="text-[#5DBA63] "/>
-          <span>Notification</span>
-        </div>
-        <div className="flex flex-col cursor-pointer">
-          <FontAwesomeIcon icon={faShoppingCart} className="text-[#5DBA63] "/>
+          <FontAwesomeIcon icon={faShoppingCart} className="text-[#5DBA63] " />
           <span>Cart</span>
         </div>
-        <div className="flex flex-col cursor-pointer">
-          <FontAwesomeIcon icon={faUser} className="text-[#5DBA63] "/>
-          <span>Profile</span>
-        </div>
+        <Link to="/profile">
+          <div className="flex flex-col cursor-pointer">
+            <FontAwesomeIcon icon={faUser} className="text-[#5DBA63] " />
+            <span>Profile</span>
+          </div>
+        </Link>
       </section>
     </nav>
   );
