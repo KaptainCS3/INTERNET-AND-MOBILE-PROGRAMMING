682b4ee78569517d2245e6c12814df8e751dd313
KaptainCS3 <mbiapplegryn@gmail.com>
2023-06-02 19:52:37 +0100

    change to navlink

--- Task 5/Eat_Now/src/components/Options.jsx
+++ Task 5/Eat_Now/src/components/Options.jsx
@@ -2,20 +2,20 @@ import { faMap } from "@fortawesome/free-regular-svg-icons";
 import { faBurger, faDonate, faMedal } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Options = () => {
   return (
     <>
       <section className="flex justify-between mx-6 my-4">
-        <Link to="/nearby">
+        <NavLink to="/nearby">
           <div className="flex flex-col items-center">
             <span className="px-[0.25rem] py-[0.25rem] border bg-white rounded-lg text-[#5DBA63]">
               <FontAwesomeIcon icon={faMap} className="text-xl" />
             </span>
             <small className="text-[0.6rem] my-[0.25rem]">Nearby</small>
           </div>
-        </Link>
+        </NavLink>
         <div className="flex flex-col items-center">
           <span className="px-[0.25rem] py-[0.25rem] border bg-white rounded-lg text-[#5DBA63]">
             <FontAwesomeIcon icon={faMedal} className="text-xl" />
