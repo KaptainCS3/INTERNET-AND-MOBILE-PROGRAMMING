ec19844c00254df18edf66ebd2d86de98cfe93b7
KaptainCS3 <mbiapplegryn@gmail.com>
2023-06-16 23:13:59 +0100

    initial commit

--- Task 5/backend/routes/login.js
+++ Task 5/backend/routes/login.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const db = require("../app/models");
 const router = express.Router();
 const FoodReceivers = db.food_receivers;
@@ -16,17 +17,22 @@ router.post("/login", async (req, res) => {
   const donor = await FoodDonors.findOne({ where: { email } });
   // If neither found, return an error response
   if (!receiver && !donor) {
-    return res.status(401).json({ message: "Invalid email or password" });
+    return res.status(401).json({ auth: false, message: "no user exists" });
   }
   // Check if the password matches for the found user
   const user = receiver || donor;
   const passwordMatch = await bcrypt.compare(password, user.password);
   // If the password doesn't match, return an error response
   if (!passwordMatch) {
-    return res.status(401).json({ message: "Invalid email or password" });
+    return res
+      .status(401)
+      .json({ auth: false, message: "Invalid email or password" });
   }
   // Password matches, user authenticated
-  res.json({ message: "Authentication successful" });
+  // Generate a JWT with the user's ID as payload
+  const token = jwt.sign({ id: user.id }, "jwtSecret", { expiresIn: 300 });
+  // Send the JWT in the response
+  res.json({ auth: true, token: token, message: "Authentication successful" });
 });
 
 module.exports = router;
