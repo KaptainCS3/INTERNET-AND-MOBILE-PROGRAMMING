de59a15b95d4471782e5784fe7d599a8d7af22f1
KaptainCS3 <mbiapplegryn@gmail.com>
2023-06-16 23:14:18 +0100

    initial commit

---
+++ Task 5/backend/routes/receiverData.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const db = require("../app/models");
+const router = express.Router();
+const allFood = db.food_receivers;
+
+router.get("/receivers", async (req, res, next) => {
+  try {
+    const products = await allFood.findAll();
+    res.json({ products });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to retrieve products" });
+  }
+});
+
+module.exports = router;
