c8fb2f046379e54747460f5c5cc8c64afa654f62
KaptainCS3 <mbiapplegryn@gmail.com>
2023-06-17 21:25:54 +0100

    initial commit

---
+++ Task 5/Eat_Now/list.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    img: "/assets/carouselList/pexels-cats-coming-2335690.jpg",
+  },
+  {
+    id: 2,
+    img: "/assets/carouselList/pexels-fox-13189291.jpg",
+  },
+  {
+    id: 3,
+    img: "/assets/carouselList/pexels-tubarones-photography-9546271.jpg",
+  },
+  {
+    id: 4,
+    img: "/assets/food_features/image1.jpg",
+  },
+];
